e5675c7937ccffd6ed05f7b45ab5bd04379e8b01
James Rode <rodej2@winthrop.edu>
2019-12-12 18:27:00 -0500

    Add unit tests
    Added some basic CLI argument- and parsing-tests.

---
+++ PhotoOrganizer.Tests/CliTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using NUnit.Framework;
+
+namespace PhotoOrganizer.Tests
+{
+    public class CliTests
+    {
+        #region LIST
+        [Test]
+        [Description("Invokes the 'list' CL-arg with an invalid directory type")]
+        public static void List_InvalidType_PrintsError()
+        {
+            const string arg_plural = "list sources";
+            const string arg_wrong = "list undertaker";
+
+            string result = Runner.RunProgram(arg_plural);
+            Assert.AreEqual("unrecognized type sources", result.ToLower(), "Program accepted the plural of a valid type");
+
+            result = Runner.RunProgram(arg_wrong);
+            Assert.AreEqual("unrecognized type undertaker", result.ToLower(), "Program accepted an invalid type");
+        }
+
+        [Test]
+        [Description("Invokes the 'list' CL-arg with a valid directory type")]
+        public static void List_ValidType_PrintsEntries()
+        {
+            const string arg_source = "list source";
+            const string arg_target = "list target";
+            const string arg_base = "list";
+
+            string result = Runner.RunProgram(arg_source);
+            Assert.AreNotEqual("unrecognized type source", result);
+
+            result = Runner.RunProgram(arg_target);
+            Assert.AreNotEqual("unrecognized type target", result);
+
+            result = Runner.RunProgram(arg_base);
+            Assert.IsFalse(result.Contains("unrecognized type")); // super bad test theoretically but practically its fine, maybe
+        }
+
+        #endregion
+    }
+}
--- PhotoOrganizer.Tests/Runner.cs
+++ PhotoOrganizer.Tests/Runner.cs
@@ -12,21 +12,59 @@ namespace PhotoOrganizer.Tests
     public class Runner
     {
         /// <summary>
-        /// Run the program
+        /// Run the program with the given array of command line arguments
         /// </summary>
         /// <param name="args"></param>
         /// <returns></returns>
-        public static string RunProgram(string[] args)
+        public static string RunProgram(string[] args, bool trim = true, bool lowercase = true)
         {
             string result = string.Empty;
+
             using (StringWriter writer = new StringWriter())
             {
                 Console.SetOut(writer);
                 Program.Main(args);
                 result = writer.ToString();
             }
-            
-            //assembly.GetType()
+
+            // Process cleaning options
+            if (trim)
+            {
+                result = result.Trim('\r', '\n', ' ');
+            }
+
+
+            if (lowercase)
+                result = result.ToLower();
+
+            return result;
+        }
+
+        /// <summary>
+        /// Run the program with the given command line argument string
+        /// </summary>
+        /// <param name="arg">String of arguments to pass to program</param>
+        /// <returns></returns>
+        public static string RunProgram(string arg, bool trim = true, bool lowercase = true)
+        {
+            string result = String.Empty;
+
+            using (StringWriter writer = new StringWriter())
+            {
+                Console.SetOut(writer);
+                Program.Main(arg.Split(' '));
+                result = writer.ToString();
+            }
+
+            // Process cleaning options
+            if (trim)
+            {
+                result = result.Trim('\r', '\n', ' ');
+            }
+
+
+            if (lowercase)
+                result = result.ToLower();
 
             return result;
         }
